741a792a9ed4fe91b267301e59d1500eb8ccef2f
joshcbrown <80245312+joshcbrown@users.noreply.github.com>
2022-01-10 09:39:58 +1100

    approx jacobian implementation

--- score_matching.py
+++ score_matching.py
@@ -28,14 +28,25 @@ class SM(nn.Module):
         
 
 def J_hat(sample: torch.Tensor, output: torch.Tensor) -> float:
-    score = grad(output, sample, create_graph=True)[0]
+    score = grad(output, sample, create_graph=True, retain_graph=True)[0]
     logging.info(score)
     trace = 0
     for i in range(len(score)):
-        trace += grad(score[i], sample[i])
+        trace += grad(score[i], sample[i], create_graph=True, retain_graph=True)
     logging.info(trace)
     return 1/2 * (score * score).sum() + trace
     
+def approx_jacobian_trace(fx, x):
+
+    eps = torch.randn_like(fx)
+    eps_dfdx = keep_grad(fx, x, grad_outputs=eps)
+    tr_dfdx = (eps_dfdx * eps).sum(-1)
+
+    return tr_dfdx
+
+def keep_grad(output, input, grad_outputs=None):
+    return torch.autograd.grad(output, input, grad_outputs=grad_outputs, retain_graph=True, create_graph=True)[0]
+
 
 def train(args, model, train_loader, optimiser, epoch):
     pass
@@ -49,12 +60,16 @@ def main():
 
     model = SM()
     sample = distr.rsample()
-    sample.requires_grad = True
+    sample.requires_grad_(True)
     logging.info(sample)
     
     output = model(sample)[0]
     logging.info(output)
-    logging.info(J_hat(sample, output))
+    score = keep_grad(output, sample)
+    logging.info(score)
+    trace = approx_jacobian_trace(score, sample)
+    logging.info(trace)
+    # logging.info(J_hat(sample, output))
     
 
 if __name__ == '__main__':
