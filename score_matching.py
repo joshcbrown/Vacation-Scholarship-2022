946f1a9970731f79acd1e2ca0ccc6e217ee9a8b1
joshcbrown <80245312+joshcbrown@users.noreply.github.com>
2022-01-10 11:55:42 +1100

    first attempt at creating a training loop

--- score_matching.py
+++ score_matching.py
@@ -1,8 +1,12 @@
 import torch
 import logging
+import numpy as np
+from tqdm import tqdm
 from torch import nn
 from torch.autograd import grad
+from torch.optim import Adam
 from torch.distributions.multivariate_normal import MultivariateNormal
+
 
 class SM(nn.Module):
     def __init__(self):
@@ -44,12 +48,42 @@ def approx_jacobian_trace(fx, x):
 
     return tr_dfdx
 
+def sliced_J_hat(output, sample):
+    score = keep_grad(output, sample)
+    trace = approx_jacobian_trace(score, sample)
+    return 1/2 * (score * score).sum() + trace
+
 def keep_grad(output, input, grad_outputs=None):
     return torch.autograd.grad(output, input, grad_outputs=grad_outputs, retain_graph=True, create_graph=True)[0]
 
 
-def train(args, model, train_loader, optimiser, epoch):
-    pass
+def train(model, distr, optimiser, training):
+    training.requires_grad = True
+    for epoch in tqdm(range(10)):
+        optimiser.zero_grad()
+        
+        outputs = model(training)
+        loss = 0
+        for output, input in zip(outputs, training):
+            loss += sliced_J_hat(output[0], input)
+        loss /= len(training)
+
+        loss.backward()
+        optimiser.step()
+    
+
+def write_samples(distribution, train_size=10000, test_size=1000):
+    training = distribution.rsample((1, train_size))[0]
+    testing = distribution.rsample((1, test_size))[0]
+    np.savetxt('data/training.txt', training.numpy())
+    np.savetxt('data/testing.txt', testing.numpy())
+
+def get_samples():
+    training = torch.Tensor(np.loadtxt('data/training.txt'))
+    training.requires_grad_(True)
+    testing = torch.Tensor(np.loadtxt('data/testing.txt'))
+    testing.requires_grad_(True)
+    return training, testing
 
 def main():
     logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -58,18 +92,21 @@ def main():
     cov = torch.Tensor([[1, 1, 1], [1, 2, 2], [1, 2, 3]])
     distr = MultivariateNormal(loc=mean, covariance_matrix=cov)
 
+    training, testing = get_samples()
+
     model = SM()
-    sample = distr.rsample()
-    sample.requires_grad_(True)
-    logging.info(sample)
+    optimiser = Adam(model.parameters(), lr = 0.9)
     
-    output = model(sample)[0]
-    logging.info(output)
-    score = keep_grad(output, sample)
-    logging.info(score)
-    trace = approx_jacobian_trace(score, sample)
-    logging.info(trace)
-    # logging.info(J_hat(sample, output))
+    train(model, distr, optimiser, training)
+    
+
+    # output = model(sample)[0]
+    # logging.info(output)
+    # score = keep_grad(output, sample)
+    # logging.info(score)
+    # trace = approx_jacobian_trace(score, sample)
+    # logging.info(trace)
+    # # logging.info(J_hat(sample, output))
     
 
 if __name__ == '__main__':
